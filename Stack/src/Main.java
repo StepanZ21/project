fa58564d0dc5d97d247075f12fa52a4dfecd1431
StepanZ21 <93367647+StepanZ21@users.noreply.github.com>
2024-02-28 18:07:42 +0900

    Add files via upload

---
+++ Stack/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        while(!stack.isEmpty()){
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
